1e5138c7d211d59f9fe7afdbb9f57bd18d08e61c
eternal golden bread <codylewisbrock@gmail.com>
2019-10-12 14:26:29 -0700

    fixes the code so the functions are stored in an object.  Adds text to the readme.  Deletes an old sandbox file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Word-Guess-Game
+
+**Intro**
+Welcome to my Word-Guess-Game!
+
+My theme is Happy Birthday.  I'm not sure why, it just felt right.
+
+
+**How it works**
+When the page is loaded, there are instructions to press any letter to start.  Once a button is pressed, the instructions disappear.  
+
+My code detects what type of key is pressed - if it's a-z, it accepts it as a guess, otherwise it notifies the player to use a-z.  It also converts from upper to lower case.  As correct guesses accumulate, the hash markers turn to the correct letters.  As incorrect guesses accumulate, they are added to the incorrect guesses array.  Duplicate guesses are ignored.  
+
+Once there are no more hashes, the victory conditions are met and an alert appears.  The wins tally is incremented, and (per instructions) the game restarts.  If you run out of guesses, then the losing conditions are met.  The victory tally is not incremented, and the game restarts.
+
+
+**Other notes**
+Per the bonus instructions, I've organized my game code as an object (for the most part).
+
+I've left fairly detailed notes within my code to help follow the logic of my program, and the lack thereof.
+
+I've also included a javascript file where the code is not in an object, which is how I originally wrote it.  The script source line is commented out on the index.html page.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,156 +14,119 @@ var winsDestination = document.getElementById('wins');
 var pleaseSelectLetter = document.getElementById('please-select-letter');
 var jumbo = document.getElementById('jumbo-tron')
 
-
-//sets and resets variables for first or new game
-function initialize() {
-    remainingIncorrectGuesses = 7;
-    correctGuesses = [];
-    incorrectGuesses = [];
-    jumbo.style.display = "block";
-    
-
-    
-
-
-
-    //selects random word
-    var hangmanWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-    console.log('hangmanWord', hangmanWord);
-
-    //breaks word into array of individual letters
-    lettersInWord = hangmanWord.split("");
-    console.log("split!", lettersInWord, lettersInWord.length);
-
-    //creates an array of hashes matching length of word
-    var hashes = [];
-    for (let i = 0; i < lettersInWord.length; i++) {
-        hashes.push('-');
-    }
-    //turns this into a string that can appear on page
-    hashesWord = hashes.join('');
-
-
-    secretHolderText.textContent = hashesWord;
-    wrongGuesses.textContent = incorrectGuesses;
-    guessesLeft.textContent = remainingIncorrectGuesses;
-    winsDestination.textContent = wins;
-
-
-
-
-};
-
-initialize();
-
-
-
-
-//Main code
-//============================================================================================
-
-//Sets up the underscore
-
-
-//when user pushes key
 var userGuess;
 
-//detects what key is pushed
-document.onkeyup = function(event) {
-    //hides the message to select a letter a-z
-    pleaseSelectLetter.style.display = 'none'
-    jumbo.style.display = 'none'
-    //if userGuess is a letter a - z...
-    if (event.keyCode >=65 && event.keyCode <=90) {
-        //set userGuess and move on with function
-        userGuess = event.key.toLowerCase();
-        console.log('userGuess', userGuess);
-    }
-    //if user guess is not a letter...
-    else if (event.keyCode <65 || event.keyCode >90) {
-        //tell user to use letters, end function
-        console.log("please select a letter between a - z");
-        pleaseSelectLetter.style.display = 'block';
-        return;
-    }
-    
-   
-    //detects if a key has already been guessed, stops code
-    if ((correctGuesses.indexOf(userGuess) > -1) || incorrectGuesses.indexOf(userGuess) > -1) {
-        console.log('You already tried that!  Please guess a new letter')
-        console.log("incorrectGuesses",incorrectGuesses);
-        return;
-    }
+var gameObj = {
+    //sets and resets variables for first or new game
+    initialize: function() {
+        remainingIncorrectGuesses = 7;
+        correctGuesses = [];
+        incorrectGuesses = [];
+        jumbo.style.display = "block";
+        
+        //selects random word
+        var hangmanWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+        console.log('hangmanWord', hangmanWord);
 
-    //detects if guess is present in word.
-        else if (lettersInWord.indexOf(userGuess) !== -1) {
-            //loops through all letters
-            for (let i=0; i<lettersInWord.length; i++) {
-                //if letter matches user guess...
-                if (lettersInWord[i] === userGuess) {
-                    //insert the guessed letter into the hashes
-                    hashesWord = hashesWord.split('');
-                    hashesWord[i] = userGuess;
-                    hashesWord = hashesWord.join('');
-                }
-            };
-            
-            
-            correctGuesses.push(userGuess);
+        //breaks word into array of individual letters
+        lettersInWord = hangmanWord.split("");
+        console.log("split!", lettersInWord, lettersInWord.length);
 
-            console.log('Eureka!');
-            console.log(correctGuesses);
+        //creates an array of hashes matching length of word
+        var hashes = [];
+        for (let i = 0; i < lettersInWord.length; i++) {
+            hashes.push('-');
+        }
+        //turns this into a string that can appear on page
+        hashesWord = hashes.join('');
 
+
+        secretHolderText.textContent = hashesWord;
+        wrongGuesses.textContent = incorrectGuesses;
+        guessesLeft.textContent = remainingIncorrectGuesses;
+        winsDestination.textContent = wins;
+    },
+    gameStart: function(event) {
+        pleaseSelectLetter.style.display = 'none'
+        jumbo.style.display = 'none'
+        //if userGuess is a letter a - z...
+        if (event.keyCode >=65 && event.keyCode <=90) {
+            //set userGuess and move on with function
+            userGuess = event.key.toLowerCase();
+            console.log('userGuess', userGuess);
+        }
+        //if user guess is not a letter...
+        else if (event.keyCode <65 || event.keyCode >90) {
+            //tell user to use letters, end function
+            console.log("please select a letter between a - z");
+            pleaseSelectLetter.style.display = 'block';
+            return;
+        }
+        
+       
+        //detects if a key has already been guessed, stops code
+        if ((correctGuesses.indexOf(userGuess) > -1) || incorrectGuesses.indexOf(userGuess) > -1) {
+            console.log('You already tried that!  Please guess a new letter')
+            console.log("incorrectGuesses",incorrectGuesses);
+            return;
         }
     
-    //if user guess is incorrect
-    else if (lettersInWord.indexOf(userGuess) === -1) {
-        //decrease remaining guesses
-        remainingIncorrectGuesses--
-        //push to incorrectGuesses
-        incorrectGuesses.push(userGuess);
-        console.log("remainingIncorrectGuesses",remainingIncorrectGuesses);
-        console.log("incorrectGuesses",incorrectGuesses);
-    }
-
-    //updates these text fields
-    secretHolderText.textContent = hashesWord;
-    wrongGuesses.textContent = incorrectGuesses;
-    guessesLeft.textContent = remainingIncorrectGuesses;
-    winsDestination.textContent = wins;
-
-
-    //Victory condition!  If there are no more hashes
-    if (hashesWord.indexOf('-') === -1) {
-        //all done!
-        alert("You Win!");
-        wins++;
-        winsDestination.textContent = wins;
-        initialize();
-    } else if (remainingIncorrectGuesses <= 0) {
-        alert("You Lose!")
-        initialize();
-    }
-
-    //     var restart = confirm("Would you like to play again?");
-    //     if (restart === true) {
-    //         initialize();
-    //     }
-    // }
-    // if (remainingIncorrectGuesses <= 0) {
-    //     alert("You lose!")
-    //     var restart = confirm("Would you like to play again?");
-    //     if (restart === true) {
-    //         initialize();
-    //     }
-    // }
+        //detects if guess is present in word.
+            else if (lettersInWord.indexOf(userGuess) !== -1) {
+                //loops through all letters
+                for (let i=0; i<lettersInWord.length; i++) {
+                    //if letter matches user guess...
+                    if (lettersInWord[i] === userGuess) {
+                        //insert the guessed letter into the hashes
+                        hashesWord = hashesWord.split('');
+                        hashesWord[i] = userGuess;
+                        hashesWord = hashesWord.join('');
+                    }
+                };
+                
+                
+                correctGuesses.push(userGuess);
+    
+                console.log('Eureka!');
+                console.log(correctGuesses);
+    
+            }
         
-
+        //if user guess is incorrect
+        else if (lettersInWord.indexOf(userGuess) === -1) {
+            //decrease remaining guesses
+            remainingIncorrectGuesses--
+            //push to incorrectGuesses
+            incorrectGuesses.push(userGuess);
+            console.log("remainingIncorrectGuesses",remainingIncorrectGuesses);
+            console.log("incorrectGuesses",incorrectGuesses);
+        }
+    
+        //updates these text fields
+        secretHolderText.textContent = hashesWord;
+        wrongGuesses.textContent = incorrectGuesses;
+        guessesLeft.textContent = remainingIncorrectGuesses;
+        winsDestination.textContent = wins;
+    
+    
+        //Victory condition!  If there are no more hashes
+        if (hashesWord.indexOf('-') === -1) {
+            //all done!
+            alert("You Win!");
+            wins++;
+            winsDestination.textContent = wins;
+            gameObj.initialize();
+        } else if (remainingIncorrectGuesses <= 0) {
+            alert("You Lose!")
+            gameObj.initialize();
+        }
+    
+    }
+        
 }
 
 
-//DOM manipulation
-//============================================================================================
-//QUESTION: does this need to be in the function?
-//should print the hashmarks to page, but is NOT WORKING
 
+gameObj.initialize();
+
+document.addEventListener('keyup', gameObj.gameStart);
